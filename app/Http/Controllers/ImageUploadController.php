bdfa62a6ad00e226b1c17d8b4efbd7eb43e9a286
NchyG <godsonnchy@gmail.com>
2019-12-15 18:46:20 -0800

    save to database

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use JD\Cloudder\Facades\Cloudder;
 use Illuminate\Http\Request;
+use App\Upload;
 
 class ImageUploadController extends Controller
 {
     public function home()
     {
-        return view('home');
+        $images = Upload::all();
+
+        return view('home', compact('images'));
     }
 
     public function uploadImages(Request $request)
@@ -18,10 +21,34 @@ class ImageUploadController extends Controller
             'image_name' => 'required|mimes:jpeg,bmp,jpg,png|between:1, 6000',
         ]);
 
+        $image = $request->file('image_name');
+
+        $name = $request->file('image_name')->getClientOriginalName();
+
         $image_name = $request->file('image_name')->getRealPath();
 
         Cloudder::upload($image_name, null);
 
+        list($width, $height) = getimagesize($image_name);
+
+        $image_url = Cloudder::show(Cloudder::getPublicId(), ["width" => $width, "height" => $height]);
+
+        // save to upload directory
+        $image->move(public_path("uploads"), $name);
+
+        // save images
+        $this->saveImages($request, $image_url);
+
         return redirect()->back()->with('status', 'Image Uploaded Successfully');
+    }
+
+
+    public function saveImages(Request $request, $image_url)
+    {
+        $image = new Upload();
+        $image->image_name = $request->file('image_name')->getClientOriginalName();
+        $image->image_url = $image_url;
+
+        $image->save();
     }
 }
